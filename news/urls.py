5d7fed35b5164416c0795923b5fc582a43861526
Ronny Kerosi <ronniemokaya30@gmail.com>
2023-08-02 12:16:53 +0300

    Implemented a CBV (list-based)
    But never finished with the details view

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import NewsList, NewsDetail
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', NewsList.as_view(), name="home"),
+    path('detail/<slug:title>', NewsList.as_view(), name="detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,11 +1,12 @@
-from typing import Any
+from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
 import requests
 from django.core.paginator import Paginator
-from django.views.generic.list import ListView
+from django.views import View
+from django.views.generic import DetailView
 
 # Create your views here.
 def home(request):
@@ -34,14 +35,52 @@ def home(request):
 
 
 # Class based view
-class NewsList(ListView):
-    template = ""
-    context_object_name = "article_new"
+class NewsList(View):
+    api = os.environ.get("news_api")
+    url = "https://newsapi.org/v2/everything?q={}&sortBy=popularity&apiKey={}"
+    template = "news/home.html"
+    # context_object_name = "article_new"
 
-    def get_queryset(self):
-        field_data = self.request.GET.get("")
+    def get(self, request):
+        search_term = request.GET.get('search')
+        articles = fetch_news_data(self.url, search_term, NewsList.api)
+
+        p = Paginator(articles, 5)
+        page_number = request.GET.get("page")
+        article_new = p.get_page(page_number)
+
+        context = {
+            # "articles": articles,
+            "article_new": article_new,
+        }
+
+        return render(request, "news/home.html", context)
+    
+
+class NewsDetail(DetailView, NewsList):
+    template_name = "news/detail.html"
+    slug_url_kwarg = "title"
+    slug_field = "title"
+    context_object_name = "article"
+    # api = os.environ.get("news_api")
+    # url = "https://newsapi.org/v2/everything?q={}&sortBy=popularity&apiKey={}"
+
+    def get_object(self, queryset=  None):
+        title = self.kwargs.get(self.slug_url_kwarg)
+        articles = fetch_news_data(self.url, self.search_term, NewsList.api)
+
+        for article in articles:
+            if article["title"] == title:
+                return article
+            
+        return None
+    
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
 
 
+def redirect_to_detail(request, title):
+    articles = fetch_news_data(url, search_term, api)
 
 
 
